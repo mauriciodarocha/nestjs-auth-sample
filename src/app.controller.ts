8f26dd878e932ebedf6c3943b2881aad20b5aff5
Mauricio da Rocha <mauriciodarocha@gmail.com>
2021-08-25 00:12:40 -0300

    Added session cookie

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthenticatedGuard } from './auth/authentication.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
@@ -9,11 +10,12 @@ export class AppController {
     @UseGuards(LocalAuthGuard)
     @Post('login')
     login(@Request() req): any {
-        return req.user;
+        return { 'message': 'User logged in.' };
     }
 
+    @UseGuards(AuthenticatedGuard)
     @Get('protected')
-    getHello(): string {
-        return this.appService.getHello();
+    getHello(@Request() req): string {
+        return req.user;
     }
 }

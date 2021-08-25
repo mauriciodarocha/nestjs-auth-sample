5d210f63dcd54e2247f3b719ddb9b045fbb197c1
Mauricio da Rocha <mauriciodarocha@gmail.com>
2021-08-24 22:05:19 -0300

    Added method findOne user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      name: 'Mauricio',
+      username: 'mauriciodarocha',
+      password: '123',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user: User) => user.username === username);
+  }
+}

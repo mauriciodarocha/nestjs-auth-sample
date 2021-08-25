3753e231ea197874b0731b4958daec94e3fbb093
Mauricio da Rocha <mauriciodarocha@gmail.com>
2021-08-24 23:20:09 -0300

    Added rest test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Post('login')
-  login(): any {
-    return {};
-  }
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    login(@Request() req): any {
+        return req.user;
+    }
 
-  @Get('protected')
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get('protected')
+    getHello(): string {
+        return this.appService.getHello();
+    }
 }
